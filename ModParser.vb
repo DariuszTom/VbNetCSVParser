3d144165c5678f8b592ea9bd724da654ede81c1f
Dariusz Tomczak <dktomczak@o2.pl>
2021-04-14 14:23:46 +0200

    Fix

--- ModParser.vb
+++ ModParser.vb
@@ -3,9 +3,21 @@ Imports System.Collections.Generic
 Imports System.Linq
 Imports System.IO
 Imports Microsoft.VisualBasic.FileIO
-Module ModParser
-
-    Function Main(path As String, nameF As String) As List(Of String())
+Imports System.Console
+Module Module1
+    Public Sub Main()
+        Dim P As String
+        Dim N As String
+        Dim obj As Object
+        WriteLine("Please input path for CSV file")
+        P = FormatPath(ReadLine())
+        WriteLine("Please input Name for CSV file")
+        N = ReadLine()
+        If Not P = vbNullString And Not N = vbNullString Then
+            obj = MainFun(P, N)
+        End If
+    End Sub
+    Function MainFun(path As String, nameF As String) As List(Of String())
         If File.Exists(path & nameF) = False Then Return Nothing
         Return VBCSVParser(path & nameF)
     End Function
@@ -30,5 +42,11 @@ Module ModParser
 
         tempList = tempList.OrderBy(Function(arr) arr(0)).ThenBy(Function(arr) arr(1)).ToList() 'Linq sort
         Return tempList
+    End Function
+    Public Function FormatPath(ByVal path As String) As String
+        Dim slash = "\" ' check if path is ending with slash
+        Dim c As Char = path.Trim().ToString().Last()
+        If Not Equals(c.ToString(), slash) Then path += slash
+        Return path
     End Function
 End Module
